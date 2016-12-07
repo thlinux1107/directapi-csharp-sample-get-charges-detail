461cb9b84c9deb2b2c0f3f5985d7bcdcb4ab1bd4
Thomas Hagan <thlinux@gmail.com>
2016-12-07 12:56:46 -0500

    Initial file upload.

---
+++ DirectAPI_Csharp_Sample/DirectAPI_Csharp_Sample/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace DirectAPI_Csharp_Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btProcess = new System.Windows.Forms.Button();
+            this.txtJSONResponse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReference = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRespCard = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRespExpDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRespPayTypeID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btProcess
+            // 
+            this.btProcess.Location = new System.Drawing.Point(11, 236);
+            this.btProcess.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btProcess.Name = "btProcess";
+            this.btProcess.Size = new System.Drawing.Size(125, 28);
+            this.btProcess.TabIndex = 1;
+            this.btProcess.Text = "Retrieve Reference";
+            this.btProcess.UseVisualStyleBackColor = true;
+            this.btProcess.Click += new System.EventHandler(this.btProcess_Click);
+            // 
+            // txtJSONResponse
+            // 
+            this.txtJSONResponse.Location = new System.Drawing.Point(195, 23);
+            this.txtJSONResponse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtJSONResponse.Multiline = true;
+            this.txtJSONResponse.Name = "txtJSONResponse";
+            this.txtJSONResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtJSONResponse.Size = new System.Drawing.Size(352, 245);
+            this.txtJSONResponse.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Response";
+            // 
+            // txtReference
+            // 
+            this.txtReference.Location = new System.Drawing.Point(11, 23);
+            this.txtReference.Name = "txtReference";
+            this.txtReference.Size = new System.Drawing.Size(162, 20);
+            this.txtReference.TabIndex = 6;
+            this.txtReference.Text = "DC6GFbyDO0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 8);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter Reference Number";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(561, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Card Number";
+            // 
+            // txtRespCard
+            // 
+            this.txtRespCard.Location = new System.Drawing.Point(561, 23);
+            this.txtRespCard.Name = "txtRespCard";
+            this.txtRespCard.Size = new System.Drawing.Size(162, 20);
+            this.txtRespCard.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(561, 51);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Expiration Date";
+            // 
+            // txtRespExpDate
+            // 
+            this.txtRespExpDate.Location = new System.Drawing.Point(561, 66);
+            this.txtRespExpDate.Name = "txtRespExpDate";
+            this.txtRespExpDate.Size = new System.Drawing.Size(162, 20);
+            this.txtRespExpDate.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(561, 96);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Payment Type ID";
+            // 
+            // txtRespPayTypeID
+            // 
+            this.txtRespPayTypeID.Location = new System.Drawing.Point(561, 111);
+            this.txtRespPayTypeID.Name = "txtRespPayTypeID";
+            this.txtRespPayTypeID.Size = new System.Drawing.Size(162, 20);
+            this.txtRespPayTypeID.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 275);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtRespPayTypeID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRespExpDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRespCard);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtReference);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtJSONResponse);
+            this.Controls.Add(this.btProcess);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Direct API C# Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btProcess;
+        private System.Windows.Forms.TextBox txtJSONResponse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReference;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRespCard;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRespExpDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRespPayTypeID;
+    }
+}
+
---
+++ DirectAPI_Csharp_Sample/DirectAPI_Csharp_Sample/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+
+namespace DirectAPI_Csharp_Sample
+{
+    //=====================================================
+    //Sample Direct API get_charges_detail request 
+    //from https://developer.sagepayments.com/bankcard/apis/get/charges/%7Breference%7D - C#.net
+    //Thomas Hagan
+    //Integration Consultant
+    //Sage Payment Solutions
+    //December 6th, 2016
+    //Application is intended for instructional use only.
+    //If you have any questions, please feel free to email
+    //the Integration Support team at sdksupport@sage.com
+    //Also, please make sure to register for API credentials
+    //at our developer portal: https://developer.sagepayments.com
+    //======================================================
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btProcess_Click(object sender, EventArgs e)
+        {
+            //TH - Test Data. This is the test account infomation we provide
+            //in the API Sandbox. Please contact us at sdksupport@sage.com if
+            //you need a unique test account and receive the Merchant ID and
+            //Merchant Key. In order to get your own Client ID and Client Secret
+            //you must register at https://developer.sagepayments.com and setup
+            //an App under My Apps. Please let us know if you have any questions.
+            string merchantId = "173859436515";
+            string merchantKey = "P1J2V8P2Q3D8";
+
+            //TH - The Client ID and Client Key should be hard coded and not displayed
+            //in the production product. These are your API credentials used for
+            //security and tracking purposes.
+            string clientId = "W8yvKQ5XbvAn7dUDJeAnaWCEwA4yXEgd";
+            string clientSecret = "iLzODV5AUsCGWGkr";
+
+            //TH - Set the variables
+            string url_main = "https://api-cert.sagepayments.com/bankcard/v1/charges/";
+            //TH - The sample includes the reference "DC6GFbyDO0", this will only work
+            //when using the test merchantId "173859436515" found in the API Sandbox.
+            string strReference = txtReference.Text;
+            string url_ref_details = url_main + strReference;
+            //TH - http://requestb.in is a good resource for troubleshooting.
+            string verb = "GET";
+            string nl = Environment.NewLine;
+
+
+            //!!!!!!!!!!!!!!!!!!!!!>>>IMPORTANT<<<!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //TH - There is no request body for a get_charge_detail request
+            //!!!!!!!!!!!!!!!!!!!!!>>>IMPORTANT<<<!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            //TH - Building timestamp and nonce. Needs to be epoch in seconds.
+            //TH - I am using the timestamp as my Nonce, but it is best to create
+            //separate, unique value.
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
+            string timestamp = t.TotalSeconds.ToString();
+            string nonce = timestamp;
+
+
+            //TH - Build the Authorization
+            string authToken = verb + url_ref_details + merchantId + nonce + timestamp;
+            byte[] hash_authToken = new HMACSHA512(Encoding.ASCII.GetBytes(clientSecret)).ComputeHash(Encoding.ASCII.GetBytes(authToken));
+            string hash64_authToken = Convert.ToBase64String(hash_authToken);
+
+            //TH - Initiate Web Request
+            var web_request = (HttpWebRequest)WebRequest.Create(url_ref_details);
+
+            //TH - Set the headers and details
+            var web_request_headers = web_request.Headers;
+            Console.WriteLine("Configuring web_request to include headers for Sage Direct API");
+            web_request_headers["clientId"] = clientId;
+            web_request_headers["merchantId"] = merchantId;
+            web_request_headers["merchantKey"] = merchantKey;
+            web_request_headers["nonce"] = nonce;
+            web_request_headers["timestamp"] = timestamp;
+            web_request_headers["authorization"] = hash64_authToken;
+            web_request.Method = verb;
+
+            //TH - Get the Response and Catch any errors.
+            try
+            {
+                var web_response = (HttpWebResponse)web_request.GetResponse();
+                Console.WriteLine(web_response.StatusDescription);
+                var datastream = web_response.GetResponseStream();
+
+                var reader = new StreamReader(datastream);
+                string responseFromServer = reader.ReadToEnd();
+                Console.WriteLine(responseFromServer);
+                reader.Close();
+                datastream.Close();
+                web_response.Close();
+
+                //TH - Display response.
+                dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(responseFromServer);
+                string strCardNumber = jsonResponse.bankcard.cardData.number.Value;
+                string strExpDate = jsonResponse.bankcard.cardData.expirationDate.Value;
+                string strPaymentType = jsonResponse.bankcard.cardData.type.Value;
+
+                txtJSONResponse.Text = "Server Response: " + web_response.StatusDescription + nl + "Direct API Response: " + responseFromServer;
+                txtRespCard.Text = strCardNumber;
+                txtRespExpDate.Text = strExpDate;
+                txtRespPayTypeID.Text = strPaymentType;
+
+
+            }
+            catch(Exception exception)
+            {
+                txtJSONResponse.Text = "Server Response: " + exception.Message;
+            }
+            
+
+
+
+
+
+
+
+        }
+    }
+}
